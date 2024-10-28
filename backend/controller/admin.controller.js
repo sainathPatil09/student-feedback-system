cda8266984cb64a9ff163b5b247d3dcc065e0d5c
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-10-28 23:40:17 +0530

    code to import student from excel

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,7 +1,10 @@
 import { adminModel } from "../model/admin.model.js";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+import csv from 'csvtojson'
+import xlsx from 'xlsx';
 import { facultyModel } from "../model/faculty.model.js";
+import { studentModel } from "../model/student.model.js";
 
 // admin signup
 export const adminSignup = async (req, res) => {
@@ -141,3 +144,51 @@ export const facultyData = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+//admin will import studentData
+export const studentData =async (req, res)=>{
+  // const jsonArray=await csv().fromFile(req.file.path);
+  // console.log(jsonArray)
+  // res.send(jsonArray)
+
+
+  try {
+    const filePath = req.file.path;
+
+    let jsonArray;
+    if (req.file.mimetype === 'text/csv') {
+      // Parse CSV file
+      jsonArray = await csv().fromFile(filePath);
+    } else if (
+      req.file.mimetype ===
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ) {
+      // Parse Excel file
+      const workbook = xlsx.readFile(filePath);
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      jsonArray = xlsx.utils.sheet_to_json(worksheet);
+    } else {
+      return res.status(400).send({ error: 'Unsupported file format' });
+    }
+
+    const savedStudents = await studentModel.insertMany(
+      jsonArray.map(student => ({
+        fullName: student.Name,
+        email: student.Email,
+        branch: student.branch,
+        studentYear: student.year,
+        studentDiv: student.div,
+        usn: student.usn,
+        phNumber: student.phNumber,
+      }))
+    );
+
+    // console.log(savedStudents);
+    res.send(savedStudents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Failed to import data' });
+  }
+}
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,11 +1,23 @@
 import express from 'express'
-import { adminLogin, adminSignup, facultyData } from '../controller/admin.controller.js'
-
+import { adminLogin, adminSignup, facultyData, studentData } from '../controller/admin.controller.js'
+import multer from 'multer'
 
 const router = express.Router()
+const storage = multer.diskStorage({
+    destination:(req, res, cb)=>{
+      cb(null,"./uploads" )
+    },
+    filename:(req, file, cb)=>{
+      cb(null, file.originalname)
+    }
+  })
+  const upload = multer({
+    storage,
+  })
 
 router.post("/adminLogin", adminLogin)
 router.post("/adminSignup", adminSignup)
 router.post("/assignFaculty", facultyData)
+router.post("/importStudent", upload.single("csvFile"), studentData)
 
 export default router
