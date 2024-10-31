b0ec2c0ce5ceb8927687452db6605256335fd992
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-10-31 08:09:25 +0530

    code of adminsignup page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <button className='border-2 p-2'>Admin Login</button>
+      <button className='border-2 p-2'>Coordinator Login</button>
+      <button className='border-2 p-2'>Faculty Login</button>
+      <button className='border-2 p-2'>Student Login</button>
+    </div>
+  )
+}
+
+export default Home
