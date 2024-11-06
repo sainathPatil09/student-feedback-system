d7cdc8c841cd0c7a39e4c30223162c56cb98751c
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-11-06 15:46:08 +0530

    code to reset status

--- frontend/src/components/CoordinatorPannel.jsx
+++ frontend/src/components/CoordinatorPannel.jsx
@@ -13,13 +13,23 @@ const CoordinatorPannel = () => {
       const response =await axios.post('/api/generate-key')
       console.log(response.data);
       setKey(response.data.studentAccessKey);
+      alert("Key generated")
+    }
+
+    const handleFeedbackStatus=async(e)=>{
+      e.preventDefault();
+      const response =await axios.put('/api/resetFeedbackStatus');
+      console.log(response)
+      alert("Feedback Status updated successfully")
+
     }
     console.log(key)
   return (
     <div>
       <Link to={'/assignFaculty'} className='border-2 p-2'>Assign Faculty</Link>
       <Link to={'/addStudent'} className='border-2 p-2'>Add Student</Link>
-      <button onClick={handleClick}>Generate Key</button>
+      <button className='border-2 p-2' onClick={handleClick}>Generate Key</button>
+      <button className='border-2 p-2' onClick={handleFeedbackStatus}>Update Feedback Status</button>
     </div>
   )
 }
