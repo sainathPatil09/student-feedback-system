dc0c6863d6d363d98a451818a82fe6a0d0b341a8
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-11-06 15:18:18 +0530

    buttons in coordinator pannel

--- frontend/src/components/CoordinatorPannel.jsx
+++ frontend/src/components/CoordinatorPannel.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useAuth } from '../context/AuthProvider'
+import { Link } from 'react-router-dom'
 
 const CoordinatorPannel = () => {
     const{authUser} = useAuth()
     console.log(authUser)
   return (
     <div>
-      this is coordinator pannel
+      <Link to={'/assignFaculty'} className='border-2 p-2'>Assign Faculty</Link>
+      <Link to={'/addStudent'} className='border-2 p-2'>Add Student</Link>
     </div>
   )
 }
